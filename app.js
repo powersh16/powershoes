4be2a36fcf9a310185181ee88742d3c4ffb3229a
abderaouf-abdellatif <rabdellatif60@gmail.com>
2021-05-25 15:32:46 +0100

    finished video 9, next 10, check video 8 because I had an error with bodyParser, Ididnt know how to solve it

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var path = require('path');
 var mongoose = require('mongoose');
 var config = require('./config/database');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+
 //connect to DB
 mongoose.connect(config.database);
 var db= mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function(){
-    console.log('Connected to MongoDB')
+    console.log('Connected to MongoDB');
 
-})
-
+});
 // Init app
 var app=express();
 
@@ -22,18 +24,60 @@ app.set('view engine', 'ejs');
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    res.send('working...');
-})
-
+// Body Parser middleware
+//
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());   
+
+// Express Session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// Express Validato middleware
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root   = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//set routes
+var pages = require('./routes/pages.js');
+var adminPages = require('./routes/admin_pages.js');
+
+app.use('/admin/pages', adminPages);
+app.use('/', pages);
+
+
+
 
 // Start the server
+
 var port = 3000;
 app.listen(port, function() {
   console.log('Server started on port ' + port); 
-})
+});
